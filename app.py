0c94786dc555f171287d1f643dd3e13d312adc4a
Douglas Pereira <sousa_douglas@live.com>
2026-03-19 22:41:11 -0300

    Add monthly consumption value calculation to app.py

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ hrs = int(input('Quantos horas de uso?: '))
 
 consumoMensal = (watts * hrs * 30) / 1000
 valor = consumoMensal * 0.87
+
 print(f'O consumo mensal é {consumoMensal} KWH/mês aproximadamente.')
 print(f'O valor do consumo será aproximadamente R${valor}.')
